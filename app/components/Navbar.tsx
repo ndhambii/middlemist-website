da5bb6c3a71f3c776b3c7084e9912e4cb5fd6821
Ndhambi Osvaldo T. Zitha <osvaldondhambi@gmail.com>
2025-08-05 00:51:19 +0200

    feat: add navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+import { navLinks } from "../constants.tsx/constants";
+
+const Navbar = () => {
+  return (
+    <div className="">
+      <nav className="flex items-center justify-between border border-pink-300 bg-gradient-to-r from-transparent via-pink-100 to-transparent rounded-full mt-5 py-3 px-8 w-[90%] mx-auto">
+        <div>
+          <Link href="/">
+            <h1 className="text-3xl">MiddleMist</h1>
+          </Link>
+        </div>
+        <div>
+          <ul className="flex ">
+            {navLinks.map((item) => {
+              return (
+                <li
+                  key={item.id}
+                  className="hover:shadow-lg hover:bg-gradient-to-r hover:from-transparent hover:via-white hover:to-pink-100 transition-colors duration-300 py-2 px-10 rounded-full"
+                >
+                  <Link href={item.url}>{item.name}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="font-bold">
+          <button className="bg-pink-500 shadow-lg text-white rounded-full py-3 px-12 bg-gradient-to-r from-pink-400 to-pink-600 hover:bg-gradient-to-r hover:from-pink-600 hover:to-pink-400 active:bg-gradient-to-r active:from-pink-800 active:to-pink-600 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-pink-300">
+            Contactar
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/constants.tsx/constants.tsx
@@ -0,0 +1,47 @@
+import { url } from "inspector";
+
+export const navLinks = [
+  {
+    id: 1,
+    name: "Acerca",
+    url: "/",
+  },
+  {
+    id: 2,
+    name: "Serviços",
+    url: "/",
+  },
+  {
+    id: 3,
+    name: "Benefícios",
+    url: "/",
+  },
+  {
+    id: 4,
+    name: "Testemunhos",
+    url: "/",
+  },
+  {
+    id: 5,
+    name: "Agendamento",
+    url: "/",
+  },
+];
+
+export const socialLinks = [
+  {
+    id: 1,
+    name: "Facebook",
+    url: "https://www.facebook.com/middlemistmaputo",
+  },
+  {
+    id: 2,
+    name: "Instagram",
+    url: "https://www.instagram.com/middlemiststudio0",
+  },
+  {
+    id: 3,
+    name: "WhatsApp",
+    url: "https://wa.me/c/258870715416",
+  },
+];
