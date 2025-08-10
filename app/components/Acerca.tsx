cde502fcbd880f466663540be5d7d381d30472da
Ndhambi Osvaldo T. Zitha <osvaldondhambi@gmail.com>
2025-08-10 21:59:47 +0200

    feat: add responsive desktop (final)

--- app/components/Acerca.tsx
+++ app/components/Acerca.tsx
@@ -18,20 +18,20 @@ const Hero = () => {
             priority
           />
         </div>
-        <h1 className="md:text-center text-4xl font-bold mb-2 mx-2 mt-5">
+        <h1 className="md:text-center md:text-6xl text-4xl font-bold mb-2 mx-2 mt-5">
           <span className="bg-gradient-to-r from-red-500 via-pink-500 to-[#f7e4d9] bg-clip-text text-transparent">
             Welcome
           </span>
           {"   "}
           to... <br />
-          <span className="px-6 mt-10 text-white bg-gradient-to-r from-[#ecac99] to-[#dca38e] rounded-sm ">
+          <span className="px-6 text-white bg-gradient-to-r from-[#ecac99] to-[#dca38e] rounded-sm ">
             MiddleMist
           </span>
         </h1>
         <p className="mx-2 font-bold text-lg text-[#f7e4d9] md:text-center mb-4">
           Depilação Feminina à Cera em Maputo
         </p>
-        <div className="cursor-pointer w-fit mx-auto bg-gradient-to-r from-pink-400 to-pink-600 text-white  py-2 px-6 rounded-full flex items-center shadow-lg xl:shadow-none hover:shadow-lg hover:bg-gradient-to-r hover:from-pink-600 hover:to-pink-400 active:bg-gradient-to-r active:from-pink-800 active:to-pink-600 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-pink-300">
+        <div className="cursor-pointer w-fit mx-auto bg-gradient-to-r from-pink-400 to-pink-600 text-white  py-2 px-6 rounded-full flex items-center shadow-lg md:text-3xl xl:shadow-none hover:shadow-lg hover:bg-gradient-to-r hover:from-pink-600 hover:to-pink-400 active:bg-gradient-to-r active:from-pink-800 active:to-pink-600 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-pink-300">
           <button className="mr-2 font-bold">
             <a target="_blank" href="https://wa.me/c/258870715416">
               Agendar Sessão{" "}
